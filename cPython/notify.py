11430283c4df48fd781b110f0bf0aa0a4efeef51
sunbx <sunbx@a01.monitor.ssp.ad.m6.youku>
2014-10-13 11:07:32 +0800

    bidinfo log process wfile dir change

--- cPython/notify.py
+++ cPython/notify.py
@@ -4,21 +4,23 @@
 import os
 from pyinotify import WatchManager, Notifier, ProcessEvent, IN_DELETE, IN_CREATE,IN_MODIFY,IN_CLOSE_WRITE
 
+path = "/opt/data/backup/exchange1.2/reportserver/bidinfo/log"
 wm = WatchManager()
-mask = IN_CREATE|IN_MODIFY|IN_CLOSE_WRITE
+mask = IN_CLOSE_WRITE
 
 class PFilePath(ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
         if event.name =='4913':
             pass
         else:
-            print "Close writable file: %s " % os.path.join(event.path, event.name)
-            filename = os.path.join(event.path, event.name)
-            os.system("python test.py %s" % filename)
+            print "Close writable file: %s " % event.name
+            filename = event.name
+	    if (filename.find("express") == -1 and filename.endswith("bidinfo.log")):
+                os.system("python bidinfo.py %s/%s" % (path,filename))
 
 if __name__ == "__main__":
     notifier = Notifier(wm, PFilePath())
-    wdd = wm.add_watch('.',mask,rec=True)
+    wdd = wm.add_watch(path,mask,rec=True)
 
     while True:
         try:
