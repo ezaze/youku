20c7eea8f8bbb2cbc4be39b6378c09b23e24ede0
sunbenxin <942171903@qq.com>
2014-10-16 15:28:12 +0800

    sync

--- cPython/ssp_click.py
+++ cPython/ssp_click.py
@@ -26,51 +26,58 @@ for line in r.readlines():
     for part in parts:
         result[part.split('=')[0]]=part.split('=')[1]
 
-    str=version+"\t"                //version
-    str+=(combine(result,"bj")+'\t') //time
-    str+=(combine(result,"s")+'\t') //ip
-    str+=(combine(result,"bf")+'\t') //dspid castid
-    if combine(result,"be") == "":  //idea
+    str=version+"\t"                #version
+    str+=(combine(result,"bj")+'\t') #time
+    str+=(combine(result,"s")+'\t') #ip
+#dspid castid
+    str+=(combine(result,"bf")+'\t')
+#idea
+    if combine(result,"be") == "":
         str+=(combine(result,"ci") + '\t')
     else:
         str+=(combine(result,"be")+'\t')
-    str+=(combine(result,"sct")+'\t') //categoryid
-    str+=(combine(result,"scs")+'\t') //subcategoryid
-    str+=(combine(result,"sv")+'\t') //vid
-    str+=(combine(result,"su")+'\t') //uid
-    str+=('2'+'\t')  //胜出平台id exchange 2
-    str+=(combine(result,"sck")+'\t')  //cookie
-    str+=' \t'     //al
-    str+=(combine(result,"bl")+'\t') //isLong
-    str+=(combine(result,"ss")+'\t') //节目id
-    str+=(combine(result,"std")+'\t') //正本视频vid
-    str+=' \t' //vl
-    str+=(combine(result,"sat")+'\t') //adposition
-    str+=' \t' //sessionid
-    str+=(combine(result,"scr")+'\t') //copyright
-    str+=' \t'  //orderid
-    str+=(combine(result,"sprd")+'\t') //exclusive package type
-    str+=' \t' //product type
-    str+=' \t' //avs
-    str+=' \t' //dp
-    str+=' \t' //device type
-    str+=' \t' //os type
-    str+=' \t' //clitent type
-    str+=' \t' //sdkid
-    str+=' \t' //offad
-    str+=(combine(result,"suri")+'\t') //loginid
-    str+=' \t' //reserved1 >dcid
-    str+=' \t' //reserved2
-    str+=' \t' //reserved3
-    str+=' \t' //reserved4
-    str+=' \t' //ext
-    str+=' \t' //hyid
-    str+=(combine(result,"spc")+'\t') //price
-    str+=(combine(result,"slog")+'\n') //logtype
+    str+=(combine(result,"sct")+'\t') #categoryid
+    str+=(combine(result,"scs")+'\t') #subcategoryid
+    str+=(combine(result,"sv")+'\t') #vid
+    str+=(combine(result,"su")+'\t') #uid
+#胜出平台id exchange 2
+    str+=('2'+'\t')
+    str+=(combine(result,"sck")+'\t')  #cookie
+    str+=' \t'     #al
+    str+=(combine(result,"bl")+'\t') #isLong
+#节目id
+    str+=(combine(result,"ss")+'\t')
+#正本视频vid
+    str+=(combine(result,"std")+'\t')
+    str+=' \t' #vl
+    str+=(combine(result,"sat")+'\t') #adposition
+    str+=' \t' #sessionid
+    str+=(combine(result,"scr")+'\t') #copyright
+    str+=' \t'  #orderid
+#exclusive package type
+    str+=(combine(result,"sprd")+'\t')
+#product type
+    str+=' \t'
+    str+=' \t' #avs
+    str+=' \t' #dp
+    str+=' \t' #device_type
+    str+=' \t' #os_type
+    str+=' \t' #clitent_type
+    str+=' \t' #sdkid
+    str+=' \t' #offad
+    str+=(combine(result,"suri")+'\t') #loginid
+    str+=' \t' #reserved1 >dcid
+    str+=' \t' #reserved2
+    str+=' \t' #reserved3
+    str+=' \t' #reserved4
+    str+=' \t' #ext
+    str+=' \t' #hyid
+    str+=(combine(result,"spc")+'\t') #price
+    str+=(combine(result,"slog")+'\n') #logtype
 
    #test
     if combine(result,"slog")=="":
-        i+=1
+        print "slog = ''"
     elif int(combine(result,"slog"))%2 == 1:
         youku.write(str)
     else:
